98c176f950f56803789dc0acd551f4502973f13a
Pat <patharryux@gmail.com>
2017-01-13 20:28:56 -0800

    Add dictionary functions

---
+++ 4_dictionaries/A_items.py
@@ -0,0 +1,8 @@
+dictionary = {
+    "foo"   : "bar",
+    "baz"   : "biz",
+    "boop"  : "oop"
+}
+
+## items() will return a list full of duples with key-value pairs
+print dictionary.items()
---
+++ 4_dictionaries/C_keys.py
@@ -0,0 +1,8 @@
+dictionary = {
+    "foo"   : "bar",
+    "baz"   : "biz",
+    "boop"  : "oop"
+}
+
+## keys() will return a list with values from the dictionary
+print dictionary.keys()
